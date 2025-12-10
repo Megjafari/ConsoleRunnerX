ef1647bab0726480e090e00944dce480a7ab507a
Megjafari <megberserker@gmail.com>
2025-12-10 10:54:57 +0100

    feat: Implement comprehensive menu system and highscore functionality

---
+++ Menus/HighscoreMenu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleRunnerX.Models;
+using ConsoleRunnerX.Services;
+using Spectre.Console;
+
+namespace ConsoleRunnerX.Menus
+{
+    public static class HighscoreMenu
+    {
+        public static void Show()
+        {
+            AnsiConsole.Clear();
+            AnsiConsole.Write(
+                new Rule("[bold yellow]Highscores Top 10[/]")
+                    .RuleStyle(Style.Parse("yellow")));
+
+            try
+            {
+                var allUsers = UserService.LoadAllUsers();
+                var table = new Table()
+                    .BorderColor(Color.Yellow)
+                    .AddColumns(
+                            new TableColumn("[bold]Rank[/]").Centered(),
+                            new TableColumn("[bold]Username[/]"),
+                            new TableColumn("[bold]Highscore[/]").Alignment(Justify.Right));
+                var rankedUsers = allUsers
+                    .OrderByDescending(u => u.HighScore)
+                    .Take(10);
+
+                int rank = 1;
+                foreach (var user in rankedUsers)
+                {
+                    table.AddRow(
+                        $"[grey]{rank++}.[/]",
+                        user.Username,
+                        $"[yellow bold]{user.HighScore}[/]"
+                    );
+                }
+                if (!rankedUsers.Any())
+                {
+                    table.AddRow("---", "[red]No highscores found![/]", "0");
+                }
+                AnsiConsole.Write(table);
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.MarkupLine($"[red]Error loading highscores: {ex.Message}[/]");
+            }
+            AnsiConsole.MarkupLine("\nPress any [green]Key[/] to go back to the menu...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Menus/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleRunnerX.Models;
+using ConsoleRunnerX.Services;
+using Spectre.Console;
+
+namespace ConsoleRunnerX.Menus
+{
+    public static class MainMenu
+    {
+        public static void Show(User loggedInUser)
+        {
+            bool running = true;
+            while (running)
+            {
+                AnsiConsole.Clear();
+                AnsiConsole.Write(
+                    new FigletText("ConsoleRunnerX")
+                        .LeftJustified()
+                        .Color(Color.Green));
+                AnsiConsole.MarkupLine($"[grey]Welcome, [/][green bold]{loggedInUser.Username}![/] [grey](Best Score: {loggedInUser.HighScore})[/]\n");
+
+                var selection = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[yellow]What would you like to do?[/]")
+                        .PageSize(10)
+                        .AddChoices(new[] {
+                            "Start Game",
+                            "Show Highscores",
+                            "Logout",
+                            "[red]Exit[/]"
+                        }));
+                switch (selection)
+                {
+                    case "Start Game":
+                        AnsiConsole.Clear();
+                        AnsiConsole.MarkupLine("[bold blue] The game starts soon...[/]");
+                        AnsiConsole.WriteLine("Implementera Game.cs nästa gång!");
+                        AnsiConsole.MarkupLine("\nPress any [green]Key[/] to go back to the menu...");
+                        Console.ReadKey(true);
+                        // Game.Run(loggedInUser); 
+                        break;
+
+                    case "Show Highscores":
+                        HighscoreMenu.Show(); // Anropar vår nya Highscore-meny
+                        break;
+
+                    case "Logout":
+                        running = false;
+                        break;
+
+                    case "[red]Exit[/]":
+                        Environment.Exit(0);
+                        break;
+                
+                }
+            }
+        }
+    }
+}
